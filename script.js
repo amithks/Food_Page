b9c5f986066d6e9f25e94338ff779fafb653c0ba
amithks <35530263+amithks@users.noreply.github.com>
2021-07-15 13:56:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function cartColor(x) {
+  x.style.width = "100px";
+  x.style.height = "10px";
+  x.style.backgroundColor = " #ff6666";
+    x.style.fontWeight = "bolder";
+  x.style.textShadow = "2px 5px 5px grey";
+}
+function cartUncolor(x) {
+  x.style.width = "100px";
+  x.style.height = "10px";
+  x.style.backgroundColor = "#ff8080";
+    x.style.fontWeight = "normal";
+  x.style.textShadow = "none";
+}
+function underline(x){
+  x.style.textDecoration = "underline #ff8080 solid 4px";
+  x.style.fontWeight = "bolder";
+  x.style.textShadow = "2px 5px 5px grey";
+}
+function ununderline(x){
+  x.style.textDecoration ="none"
+  x.style.fontWeight = "normal";
+  x.style.textShadow = "none"
+}
+function aboutunderline(x){
+  x.style.textDecoration ="line-through #ff8080";
+  x.style.fontWeight = "normal";
+  x.style.textShadow = "none"
+}
+function imageUnderline(x){
+  x.style.textDecoration = "underline #ff8080 solid 4px";
+  x.style.fontWeight = "bolder";
+    x.style.textShadow = "2px 5px 5px grey";
+}
+function imageNoUnderline(x){
+  x.style.textDecoration ="none"
+  x.style.fontWeight = "700";
+  x.style.textShadow = "none"
+}
+
